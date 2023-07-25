38700c314e0d90ead1b13e14a59e67299909f4c8
Jack Ohrn <jackohrn1@gmail.com>
2023-07-25 11:53:36 -0400

    project page

--- careerPortfolio/src/Components/Projects.jsx
+++ careerPortfolio/src/Components/Projects.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Projects = () => {
-  return <div style={{ height: "999px" }}>Projects</div>;
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Bayon&display=swap');
+  </style>;
+  return (
+    <>
+      <div style={{ display: "block", textAlign: "center" }}>
+        <h1
+          style={{
+            fontFamily: "Bayon",
+            color: "#FF00FF",
+            textAlign: "center",
+            fontSize: "2.7rem",
+          }}
+        >
+          Projects
+        </h1>
+      </div>
+    </>
+  );
 };
 
 export default Projects;
